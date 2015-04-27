1857f29171022e69c3079d88ebc1766d5d13552a
austin_abhari <ada72@nau.edu>
2015-04-26 23:48:46 -0700

    working on shows

--- shows.php
+++ shows.php
@@ -1,22 +1,65 @@
 <!DOCTYPE html>
+<style type="text/css">
+	.shows {
+		height: 10px;
+		width: 200px;
+		background-color: #C0C0C0;
+		border:1px solid #C0C0C0;
+	}
+
+	.shows p {
+		text-align: center;
+		font-family: 'Century Gothic', CenturyGothic, AppleGothic, sans-serif;
+		font-size: 9px;
+		top: 0px;
+		color: 0;
+		margin-top: 0;
+		letter-spacing: 2px;
+	}
+	
+	.show-content{
+		bottom:0;
+		position: absolute;
+		height:95%;
+		width:100%;
+		background-color: #F6F4EC;
+	}
+	
+	.show-header{
+		top:0;
+		position: absolute;
+		height: 5%;
+		width:100%;
+	}
+</style>
 <html>
 	<head>
 		<link rel= "stylesheet" type="text/css" href = "css/main.css">
 		<title> Flight Of Ryan </title>
 		<?php
-			include 'content/header.php';
+		include 'content/header.php';
 		?>
 	</head>
 	<body>
-			<?php
-			include 'content/slidebar.php';
-			?>
+		<?php
+		include 'content/slidebar.php';
+		?>
 		<div class = "container">
+
 			<div id="main-info">
-				
+				<center>
+					<div class= "shows">
+						<p>
+							<b>Upcoming shows</b>
+						</p>
+					</div>
+				</center>
+				<div class= "show-content">
+					<div class-=
+				</div>
 			</div>
 			<?php
-				include 'content/footer.php';
+			include 'content/footer.php';
 			?>
 		</div>
 	</body>
